d8bc3d8a077d007ddf3aaf04672a03e0a64c0171
viktor4o4o <viki1992viki47@gmail.com>
2017-04-29 22:53:12 +0300

    RegEx

---
+++ Regular Expressions (RegEx) - Lab/01. Replace a tag/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _01.Replace_a_tag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pattern = @"<a.+?href=(.+?)>(.+?)<\/a>";
+            var URL = Console.ReadLine();
+            var regex = new Regex(pattern);
+            var replacement = @"[URL href=$1]$2[/URL]";
+            while (URL != "end")
+            {
+                var result = regex.Replace(URL, replacement);
+                Console.WriteLine(result);
+                URL = Console.ReadLine();
+            }
+        }
+    }
+}
