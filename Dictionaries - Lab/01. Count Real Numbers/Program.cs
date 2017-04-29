2ef731cae4c581be58e4ce34d0200819327b6976
viktor4o4o <viki1992viki47@gmail.com>
2017-04-29 22:45:42 +0300

    dictionary

---
+++ Dictionaries - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            var result = new SortedDictionary<double, int>();
+            foreach (var number in numbers)
+            {
+                if (!result.ContainsKey(number))
+                {
+                    result[number] = 0;
+                }
+                result[number]++;
+            }
+            foreach (var kvp in result)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value} times");
+            }
+        }
+    }
+}
---
+++ Dictionaries - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine()
+                .ToLower()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if (!result.ContainsKey(word))
+                {
+                    result[word] = 0;
+                }
+                result[word]++;
+            }
+            var oddNumberWords = new List<string>();
+            foreach (var kvp in result)
+            {
+                var value = kvp.Value;
+                if (value % 2 != 0)
+                {
+                    oddNumberWords.Add(kvp.Key);
+                }
+            }
+            Console.WriteLine(string.Join(", ", oddNumberWords));
+        }
+    }
+}
