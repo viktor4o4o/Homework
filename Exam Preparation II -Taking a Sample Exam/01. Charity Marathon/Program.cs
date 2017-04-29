a77bff292f76e81c187e8cb3d4e4e24e79b76169
viktor4o4o <viki1992viki47@gmail.com>
2017-04-29 22:49:03 +0300

    prep

---
+++ Exam Preparation II -Taking a Sample Exam/01. Charity Marathon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Charity_Marathon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int marathonLenght = int.Parse(Console.ReadLine());
+            long runners = long.Parse(Console.ReadLine());
+            long averageLapsPerRunner = long.Parse(Console.ReadLine());
+            long trackLenght = long.Parse(Console.ReadLine());
+            int trackCapacity = int.Parse(Console.ReadLine());
+            double moneyPerKm = double.Parse(Console.ReadLine());
+
+            var runnersPerDay = runners;
+            if (runners > trackCapacity)
+            {
+                runnersPerDay = trackCapacity;
+            }
+            long totalrunners = marathonLenght * runnersPerDay;
+            if (totalrunners > runners)
+            {
+                totalrunners = runners;
+            }
+            var distanceInKm = (totalrunners * averageLapsPerRunner * trackLenght) / 1000;
+
+            var moneyRaised = distanceInKm * moneyPerKm;
+            Console.WriteLine("Money raised: {0:F2}", moneyRaised);
+        }
+    }
+}
---
+++ Exam Preparation II -Taking a Sample Exam/03. Nether Realms/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _03.Nether_Realms
+{
+    public class Demon
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public double Damage { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demons = Console.ReadLine()
+                .Split(new[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = new SortedDictionary<string, Demon>();
+
+            foreach (var demon in demons)
+            {
+                var health = 0;
+                var healthSymbols = demon
+                    .Where(s => !char.IsDigit(s) 
+                    && s != '+' 
+                    && s != '-' 
+                    && s != '*' 
+                    && s != '/' 
+                    && s != '.');
+                foreach (var healthSymbol in healthSymbols)
+                {
+                    health += healthSymbol;
+                }
+
+                var damage = 0.0;
+                var regex = new Regex(@"-?\d+\.?\d*");
+                var matches = regex.Matches(demon);
+                foreach (Match match in matches)
+                {
+                    var currentNum = double.Parse(match.Value);
+
+                    damage += currentNum;
+                }
+                var modSymbols = demon
+                    .Where(s => s == '*' || s == '/');
+                foreach (var mod in modSymbols)
+                {
+                    if (mod == '*')
+                    {
+                        damage = damage * 2;
+                    }
+                    else if (mod == '/')
+                    {
+                        damage = damage / 2;
+                    }
+                }
+
+                result.Add(demon, new Demon
+                {
+                    Name = demon,
+                    Health = health,
+                    Damage = damage
+                });         
+            }
+            foreach (var demonEntry  in result)
+            {
+                var demon = demonEntry.Value;
+                Console.WriteLine($"{demonEntry.Key} - {demon.Health} health, {demon.Damage:F2} damage");
+            }
+        }
+    }
+}
