0c2cc595d33728bf78bdf4ddb69a571409b7b1e8
viktor4o4o <viki1992viki47@gmail.com>
2017-04-29 22:41:57 +0300

    debugging

---
+++ Debugging and Troubleshooting Code - Lab/01. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _01.Multiply_Evens_by_Odds
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            long result = GetOddEvenMultiplication(num);
+            Console.WriteLine(result);
+        }
+        static long GetOddEvenMultiplication(int num)
+        {
+            long result = GetOddSum(num) * GetEvenSum(num);
+            return result;
+        }
+        static long GetOddSum(int num)
+        {
+            num = Math.Abs(num);
+            long sum = 0;
+            while (num > 0)
+            {
+                var digit = num % 10;
+                if (digit % 2 != 0)
+                {
+                    sum = sum + digit;
+                }
+                num /= 10;
+            }
+            return sum;
+        }
+        static long GetEvenSum(int num)
+        {
+            num = Math.Abs(num);
+            long sum = 0;
+            while (num > 0)
+            {
+                var digit = num % 10;
+                if (digit % 2 == 0)
+                {
+                    sum = sum + digit;
+                }
+                num /= 10;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Debugging and Troubleshooting Code - Lab/ConsoleApplication1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class PriceChangeAlert
+{ 
+
+    static void Main()
+    {
+        int inputNumbers = int.Parse(Console.ReadLine());
+        double allowedBorder = double.Parse(Console.ReadLine());
+        double lastPrice = double.Parse(Console.ReadLine());
+
+        for (int i = 0; i < inputNumbers - 1 ; i++)
+        {
+            double price = double.Parse(Console.ReadLine());
+
+            double difference = Percentage(lastPrice, price);
+            bool trueOrFalse = GetDifference(difference, allowedBorder);
+            string message = GetMessage(price, lastPrice, difference, trueOrFalse);
+
+            Console.WriteLine(message);
+            lastPrice = price;
+        }
+    }
+    static string GetMessage(double price, double lastPrice, double difference, bool trueOrFalse)
+    {
+        string message = string.Empty;
+        if (difference == 0)
+        {
+            message = string.Format("NO CHANGE: {0}", price);
+        }
+        else if (!trueOrFalse)
+        {
+            message = string.Format("MINOR CHANGE: {0} to {1} ({2:F2}%)", lastPrice, price, difference * 100);
+        }
+        else if (trueOrFalse && (difference > 0))
+        {
+            message = string.Format("PRICE UP: {0} to {1} ({2:F2}%)", lastPrice, price, difference * 100);
+        }
+        else if (trueOrFalse && (difference < 0))
+            message = string.Format("PRICE DOWN: {0} to {1} ({2:F2}%)", lastPrice, price, difference * 100);
+        return message;
+    }
+    static bool GetDifference(double difference, double allowedBorder)
+    {
+        if (Math.Abs(difference) >= allowedBorder)
+        {
+            return true;
+        }
+        return false;
+    }
+    static double Percentage(double lastPrice, double Price)
+    {
+        double result = (Price - lastPrice) / lastPrice ;
+        return result;
+    }
+}
